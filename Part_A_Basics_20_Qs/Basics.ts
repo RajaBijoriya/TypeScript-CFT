582894eb54572c64dc58875e750b3d1043463e08
RajaBijoriya <rajabijoriya875@gmail.com>
2025-10-20 21:35:53 +0530

    first commit

---
+++ Part_A_Basics_20_Qs/Basics.ts
@@ -0,0 +1,26 @@
+// 1. Declare a variable age of type number and assign it a value.
+// let age: number = 20;
+// console.log(age)
+
+
+// 2. Create a variable that can hold either a string or a number.
+
+// let val: number | string = "hello";
+// console.log(val)
+
+//  3. Define a tuple with a string and a boolean.
+// let arr : (string | boolean)[] = ["hello", true, "how are you", false];
+// console.log(arr)
+
+
+//  4. Write an enum Role with values Admin , User , Guest .
+
+// enum Role {
+//     Admin, 
+//     User, 
+//     Guest
+// }
+
+// console.log(Role.Admin)
+// console.log(Role.User)
+// console.log(Role.Guest)
